c73186dd4ef2fa5842cb2d0f176da838683d3ec6
rplinzner <rplinzner@gmail.com>
2020-03-21 15:36:07 +0100

    [Fix] Histogram/image placement

---
+++ ImageProcessing.View/Converters/BootToVerticalAlignmentConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ImageProcessing.View.Converters
+{
+    [ValueConversion(typeof(bool), typeof(VerticalAlignment))]
+    public class BootToVerticalAlignmentConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? VerticalAlignment.Stretch : VerticalAlignment.Top;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {    // Don't need any convert back
+            return null;
+        }
+    }
+}
